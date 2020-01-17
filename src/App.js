1884c424507ef58f49176db05265b2b29334fa44
Emiel van Rijn <mail@emielvanrijn.nl>
2020-01-17 20:05:26 +0100

    Connect app to firebase (and ignore config)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import firestore from "./firestore";
 
 function App() {
+  const db = firestore.firestore().collection("data");
+  const id = Date.now().toString();
+  db.doc(id).set({
+    initialized: true
+  });
   return <p>Hello World!</p>;
 }
 
