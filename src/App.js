935556cc663df3131315fbc58c2f3cd1dd97d3d0
Emiel van Rijn <mail@emielvanrijn.nl>
2020-01-19 13:47:44 +0100

    Add styling to App and react-bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import StateContext from "./context";
 import PageRouter from "./PageRouter";
+import "./App.scss";
 
 function App() {
   return (
     <StateContext.Provider value={0}>
-      <PageRouter />
+      <div className="App">
+        <PageRouter />
+      </div>
     </StateContext.Provider>
   );
 }
